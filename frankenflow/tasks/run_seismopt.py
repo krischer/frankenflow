4d6617d3b477c6919f7b0d876017f5cbeae786fb
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-11-11 15:08:37 +0100

    Node running seismopt.

---
+++ frankenflow/tasks/run_seismopt.py
@@ -0,0 +1,44 @@
+import os
+
+from . import task
+
+
+class RunSeismOpt(task.Task):
+    """
+    Node running seismopt.
+
+    Its pretty dumb. It just run seismopt and then returns to the
+    orchestration node.
+    """
+    @property
+    def required_inputs(self):
+        return set()
+
+    def check_pre_staging(self):
+        self.executable = os.path.join(self.context["seismopt_dir"],
+                                       "optlib.exe")
+        assert os.path.exists(self.executable)
+
+    def stage_data(self):
+        pass
+
+    def check_post_staging(self):
+        pass
+
+    def run(self):
+        cmd = [self.executable]
+        returncode = self._run_external_script(
+            cwd=self.context["seismopt_dir"],
+            cmd=cmd)
+        assert returncode == 0, \
+            "seismopt exited with return code %i." % returncode
+
+    def check_post_run(self):
+        pass
+
+    def generate_next_steps(self):
+        return [
+            {"task_type": "Orchestration",
+             "priority": 0
+            }
+        ]
