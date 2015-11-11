4c74c0863553e4d7b02e315cdde841da2d37839a
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-11-11 08:27:30 +0100

    Cleaning up the code base.

--- frankenflow/blub.py
+++
@@ -1,5 +0,0 @@
-from job_starter import run_job
-
-
-run_job("/bin", ["sleep", "10"],
-        "stdout.txt", "stderr.txt", "pid.txt")
--- frankenflow/job.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Job():
-    def __init__(self, base_folder):
-        os.makedirs(base_folder, exists_ok=True)
-        self.base_folder = base_folder
